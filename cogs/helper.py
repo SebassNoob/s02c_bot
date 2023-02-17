72cdf49fd2917c0f594775fb18b7e17cd167dec3
Sebastian Ong <78428559+SebassNoob@users.noreply.github.com>
2023-02-17 07:34:09 +0000

    add help command

--- cogs/helper.py
+++ cogs/helper.py
@@ -8,6 +8,8 @@ class Helper(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
+    
+  
   @app_commands.command(name = "customrole", description = "Give yourself a custom role!")
   @app_commands.describe(colour="The hex code of your role colour", name = "The name of your custom role")
   async def color(self, interaction: discord.Interaction, colour: app_commands.Range[str, 6, 6], name: str):
@@ -20,9 +22,13 @@ class Helper(commands.Cog):
 
     role = await interaction.guild.create_role(name=name, color = int(colour, 16))
     await interaction.user.add_roles(role)
-    await interaction.response.send_message(f"created role {name} with {colour}")
+    await interaction.response.send_message(f"created role {name} with hex code {colour}")
 
-
+  @app_commands.command(name = 'help', description = 'displays a help menu')
+  async def help(self, interaction: discord.Interaction):
+    res = discord.Embed(color=0x000000).set_author(name='s02c help')
+    res.add_field(name = "/customrole (color) (name)", value = "give yourself a custom colour role!")
+    await interaction.response.send_message(embed = res)
 
 async def setup(bot):
   await bot.add_cog(Helper(bot))
